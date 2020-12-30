707e648afb923e0550cfa54dc8eb2a9351bad215
duhl <duhl1999@outlook.com>
2020-12-30 08:54:29 +0800

    a

--- issue.sh
+++ issue.sh
@@ -9,3 +9,8 @@ git add -A .
 git commit -m "a"
 git push origin hexo
 
+echo "#######################################"
+echo " "
+echo "              finished!                "
+echo " "
+echo "#######################################"
